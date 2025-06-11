63bba439beba429217f33829505e72db9bda7603
chatterbox-house <tony@chatterboxhouse.com>
2025-06-11 13:26:56 +0900

    Create vocab.js

---
+++ vocab.js
@@ -0,0 +1,7 @@
+const vocab = [
+  { emoji: "🍎", word: "manzana", type: "noun" },
+  { emoji: "🐱", word: "gato", type: "noun" },
+  { emoji: "🎤", word: "cantar", type: "verb" },
+  { emoji: "📏", word: "alto", type: "adjective" },
+  // Add more from Wiktionary Spanish frequency list
+];
